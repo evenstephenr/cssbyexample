0fc5e73024562afacd6615357a1414048dd63859
Stephen Ramirez <github@ramirs.com>
2022-02-06 14:57:44 -0800

    update clean script

--- scripts/clean.js
+++ scripts/clean.js
@@ -2,5 +2,18 @@ const fs = require("fs");
 const path = require('path');
 
 const public = path.join(__dirname, '../docs');
-console.log('INFO: removing directory ' + public);
-fs.rm(public, { recursive: true, force: true }, () => null);
+const OVERRIDES = [
+  'CNAME',
+];
+
+console.log('INFO: cleaning directory ' + public);
+
+fs.readdirSync(public).forEach(file => {
+  const filepath = path.join(public, file);
+  if (!OVERRIDES.includes(file)) {
+    console.log('INFO: deleting file', filepath);
+    fs.rm(filepath, { force: true }, () => null);
+  } else {
+    console.log('INFO: preserving file', filepath);
+  }
+});
